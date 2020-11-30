7604e1789a248f010efd715b42e90b69ceff0a88
Sergey Semyonov <sergey.semyonov.258@gmail.com>
2020-12-01 00:51:02 +0700

    Delete unused method services.transfer_service.TransferService.get_transfers.

--- services/transfer_service.py
+++ services/transfer_service.py
@@ -10,9 +10,3 @@ class TransferSerivce:
             cursor = await conn.execute(transfer.insert().values(**data))
             transfer_id = await cursor.scalar()
         return transfer_id
-
-    async def get_transfers(self, limit_id: int):
-        async with self.__db.acquire() as conn:
-            cursor = await conn.execute(transfer.select().where(transfer.c.limit_id == limit_id))
-            records = await cursor.fetchall()
-        return records
